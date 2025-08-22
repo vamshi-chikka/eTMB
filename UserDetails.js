9bb66038dde0237bc61e5ebea11176b58f6f5216
vamshi-chikka <58411245+vamshi-chikka@users.noreply.github.com>
2025-08-22 22:21:03 +0530

    Update UserDetails.js

--- UserDetails.js
+++ UserDetails.js
@@ -3,7 +3,15 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: String,
   employeeId: String,
-  email: String
+  email: String,
+  startdate: String,
+  startdate1: String,
+  enddate: String,
+  enddate1: String,
+  id: String,
+  route: String,
+  route1: String,
+  busstopname: String 
 });
 
 // 👇 Make sure the third argument matches your collection name in Atlas (case-sensitive)
